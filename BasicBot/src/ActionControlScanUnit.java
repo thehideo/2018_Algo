45294728ddc612f0ecb3716a3fcb2b7f3a65e2e7
qksqhs <@DESKTOP-0TS1II5>
2018-07-24 20:42:55 +0900

    베슬 이동 수정

--- BasicBot/src/ActionControlScanUnit.java
+++ BasicBot/src/ActionControlScanUnit.java
@@ -4,10 +4,13 @@ public class ActionControlScanUnit implements ActionInterface {
 	@Override
 	public void action() {
 		for (Unit unit : MyVariable.scanUnit) {
-			if (MyVariable.mostFarTank != null) {
-				CommandUtil.move(unit, MyVariable.mostFarTank.getPosition());
-			} else if (unit.isIdle() == true && MyVariable.attackedUnit.size() > 0) {
+			// 공격 받는 유닛 쪽으로 이동(자기 자신이 아니면)
+			if (MyVariable.attackedUnit.size() > 0 && MyVariable.attackedUnit.get(0) != unit) {
 				CommandUtil.move(unit, MyVariable.attackedUnit.get(0).getPosition());
+			}
+			// 그렇지 않으면 가장 멀리있는 탱크 위치로 이동
+			else if (MyVariable.mostFarTank != null) {
+				CommandUtil.move(unit, MyVariable.mostFarTank.getPosition());
 			}
 		}
 	}
