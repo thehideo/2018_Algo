5db838c48e64d21d45e23fb3a67f18c3bb7bcb58
qksqhs <@DESKTOP-0TS1II5>
2018-08-04 16:06:36 +0900

    ...

--- BasicBot/src/ControlVulture.java
+++ BasicBot/src/ControlVulture.java
@@ -9,6 +9,7 @@ public class ControlVulture extends ControlAbstract {
 	BaseLocation bl = InformationManager.Instance().getFirstExpansionLocation(MyBotModule.Broodwar.self());
 
 	public void actionMain(Unit unit, GroupAbstract groupAbstract) {
+		// Spinder Mines 사용
 		if (MyUtil.distanceTilePosition(MyVariable.myStartLocation, unit.getTilePosition()) > MyUtil.distanceTilePosition(MyVariable.myStartLocation, MyVariable.myFirstchokePoint)) {
 			if (bl != null && MyUtil.distanceTilePosition(unit.getTilePosition(), bl.getTilePosition()) > 6) {
 				if (unit.canUseTech(TechType.Spider_Mines, unit.getPosition())) {
@@ -25,18 +26,17 @@ public class ControlVulture extends ControlAbstract {
 			}
 		}
 
-		if (MyVariable.isFullScaleAttackStarted == false && unit.getSpiderMineCount() > 0 && unit.canUseTech(TechType.Spider_Mines, unit.getPosition()) && MyVariable.enemyAttactUnit.size() == 0) {
-			for (TilePosition tilePosition : MyVariable.enemyBuildingUnit) {
-				if (unit.getHitPoints() == UnitType.Terran_Vulture.maxHitPoints()) {
-					CommandUtil.attackMove(unit, tilePosition.toPosition());
+		if (groupAbstract == GroupManager.instance().groupAttack) {
+			if (MyVariable.isFullScaleAttackStarted == false && unit.getSpiderMineCount() > 0 && unit.canUseTech(TechType.Spider_Mines, unit.getPosition()) && MyVariable.enemyAttactUnit.size() == 0) {
+				for (TilePosition tilePosition : MyVariable.enemyBuildingUnit) {
+					if (unit.getHitPoints() == UnitType.Terran_Vulture.maxHitPoints()) {
+						CommandUtil.attackMove(unit, tilePosition.toPosition());
+					}
+					break;
 				}
-				break;
-
 			}
 		}
 
-		// if (MyVariable.enemyAttactingUnit.size() > 0) {
 		CommandUtil.attackMove(unit, groupAbstract.getTargetPosition(unit));
-		// }
 	}
 }
