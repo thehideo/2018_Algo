b7eba24c77a3f53b0b3a5e20f4f64c9189047f38
qksqhs <@DESKTOP-0TS1II5>
2018-08-17 00:31:48 +0900

    ...

--- BasicBot/src/GroupWorker.java
+++ BasicBot/src/GroupWorker.java
@@ -9,19 +9,26 @@ public class GroupWorker extends GroupAbstract {
 		int enemyCnt = MyVariable.enemyUnitAroundMyStartPoint.size();
 		// 적의 숫자가 많으면 SCV를 동원한다. (초반에만 동작)
 		// 테란은 사용할일 없다. 사용하다가 다 죽음
-		if (InformationManager.Instance().enemyRace != Race.Terran &&  selfCnt < enemyCnt * 1.5 && selfCnt < 10 && MyBotModule.Broodwar.getFrameCount() < 12000 && MyVariable.distanceOfMostCloseEnemyUnit < MyVariable.distanceOfMostCloseBunker + 3) {
+
+		boolean useWorker = false;
+
+		if (InformationManager.Instance().enemyRace != Race.Terran && selfCnt < enemyCnt * 1.5 && selfCnt < 10 && MyBotModule.Broodwar.getFrameCount() < 12000 && MyVariable.distanceOfMostCloseEnemyUnit < MyVariable.distanceOfMostCloseBunker + 3) {
+			useWorker = true;
+		} 
+		// 일꾼이 오면 공격한다.
+		else if (InformationManager.Instance().enemyRace == Race.Terran && enemyCnt == 1 && MyVariable.getEnemyUnit(UnitType.Terran_Vulture).size() == 0) {
+			useWorker = true;
+		}
+
+		if (useWorker) {
 			int cnt = 0;
 			for (Unit unit : MyVariable.getSelfUnit(UnitType.Terran_SCV)) {
 				double distance1 = 50;
-
 				if (MyVariable.mostCloseBunker != null) {
 					distance1 = MyUtil.distanceTilePosition(MyVariable.myStartLocation.getPoint(), MyVariable.mostCloseBunker.getTilePosition()) + 3;
 				}
 				double distance2 = MyUtil.distanceTilePosition(MyVariable.myStartLocation.getPoint(), unit.getPoint().toTilePosition());
-
-				if (distance1 <= distance2) {
-					// WorkerManager.Instance().
-				} else {
+				if (distance1 > distance2) {
 					cnt++;
 					if (cnt >= MyVariable.enemyUnitAroundMyStartPoint.size() * 2 || cnt >= MyVariable.getSelfUnit(UnitType.Terran_SCV).size() - 4) {
 						break;
@@ -37,7 +44,6 @@ public class GroupWorker extends GroupAbstract {
 							}
 						}
 					}
-
 					if (enemyUnit != null) {
 						mapTargetUnit.put(unit.getID(), enemyUnit);
 					}
